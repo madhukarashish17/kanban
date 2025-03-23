08cadc09d67ab4b02871cc9f580134b7e8d94903
Ashish Madhukar <madhukar.ashish17@gmail.com>
2025-03-23 18:17:31 +0530

    UI Building and accepting input on button click

---
+++ kanban/website/script.js
@@ -0,0 +1,50 @@
+function addTask(columnId){
+    const input = document.getElementById(`${columnId}-input`);
+    const taskText=input.value.trim();
+
+    if(taskText === ""){
+        return;
+    }
+    else{
+        const taskElement = createTaskElement(taskText);
+        document.getElementById(`${columnId}-tasks`).appendChild(taskElement); 
+    }
+
+    input.value = "";
+}
+
+// document.getElementById("todo-input").addEventListener("keydown", function(event){
+//     if(event.key ==="Enter"){
+//         addTask("todo");
+//     }
+// });
+// document.getElementById("doing-input").addEventListener("keydown", function(event){
+//     if(event.key ==="Enter"){
+//         addTask("doing");
+//     }
+// });
+// document.getElementById("done-input").addEventListener("keydown", function(event){
+//     if(event.key ==="Enter"){
+//         addTask("done");
+//     }
+// });
+
+
+["todo", "doing", "done"].forEach(columnId => {
+    document.getElementById(`${columnId}-input`).addEventListener("keydown", event => {
+        if (event.key === "Enter") 
+           {
+            addTask(columnId);
+           }
+    });
+});
+// --------------------------------------------------------------------------------------------------------shorthand of above code
+
+
+function createTaskElement(taskText){
+    const element = document.createElement("div");
+    element.classList.add("taskCard");
+    element.innerText = taskText;
+    return element;
+}
+
